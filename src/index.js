5224c1c26b258a1769dfff523331b0b6df5ce4f2
Rodion Mescheryakov <msrodion@gmail.com>
2020-07-12 23:52:41 +0300

    Create basic layout of app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>It works!</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
